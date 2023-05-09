b32acb971b6ee1b64d10cbcfbbd312aa60c2cfda
deflo <deflomick@hotmail.it>
2023-05-09 19:47:45 +0200

    v1

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -10,11 +10,11 @@ class ToDo{
 });
   static List<ToDo> todoList(){
     return [
-      ToDo(id: '01', todoText: 'Esercizi' , isDone: true),
-      ToDo(id: '02', todoText: 'buy groceries' , isDone: true),
-      ToDo(id: '03', todoText: 'check email' , ),
-      ToDo(id: '04', todoText: 'team meeting' , ),
-      ToDo(id: '05', todoText: 'dinner with jenny' , ),
+      ToDo(id: '01', todoText: 'Prima nota di esempio' , isDone: true),
+      // ToDo(id: '02', todoText: 'buy groceries' , isDone: true),
+      // ToDo(id: '03', todoText: 'check email' , ),
+      // ToDo(id: '04', todoText: 'team meeting' , ),
+      // ToDo(id: '05', todoText: 'dinner with jenny' , ),
 
 
     ];
